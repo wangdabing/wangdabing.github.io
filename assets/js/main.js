0577c3d367b4681f1bef6937eec6a6323967c7ef
dabing wang <dabing wang>
2017-04-09 16:53:36 +0800

    tree

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,4 @@
   var zTree;
-  var demoIframe;
 
   var setting = {
       view: {
@@ -16,105 +15,22 @@
           }
       },
       callback: {
-          beforeClick: function(treeId, treeNode) {
-              var zTree = $.fn.zTree.getZTreeObj("tree");
+          onClick: function(event, treeId, treeNode) {
+              var zTree = $.fn.zTree.getZTreeObj(treeId);
               if (treeNode.isParent) {
                   zTree.expandNode(treeNode);
-                  return false;
-              } else {
-                  demoIframe.attr("src", treeNode.file + ".html");
-                  return true;
               }
+
+              $(".content").find("[data-id='" + treeNode.id + "']").each(function() {
+                  $(window).scrollTop($(this).scrollTop())
+              })
           }
       }
   };
 
-  var zNodes = [{
-          id: 1,
-          pId: 0,
-          name: "[core] 基本功能 演示",
-          open: true
-      }, {
-          id: 101,
-          pId: 1,
-          name: "最简单的树 --  标准 JSON 数据",
-          file: "core/standardData"
-      }, {
-          id: 102,
-          pId: 1,
-          name: "最简单的树 --  简单 JSON 数据",
-          file: "core/simpleData"
-      }, {
-          id: 103,
-          pId: 1,
-          name: "不显示 连接线",
-          file: "core/noline"
-      }, {
-          id: 104,
-          pId: 1,
-          name: "不显示 节点 图标",
-          file: "core/noicon"
-      }, {
-          id: 105,
-          pId: 1,
-          name: "自定义图标 --  icon 属性",
-          file: "core/custom_icon"
-      }, {
-          id: 106,
-          pId: 1,
-          name: "自定义图标 --  iconSkin 属性",
-          file: "core/custom_iconSkin"
-      }, {
-          id: 107,
-          pId: 1,
-          name: "自定义字体",
-          file: "core/custom_font"
-      }, {
-          id: 115,
-          pId: 1,
-          name: "超链接演示",
-          file: "core/url"
-      }, {
-          id: 108,
-          pId: 1,
-          name: "异步加载 节点数据",
-          file: "core/async"
-      }, {
-          id: 109,
-          pId: 1,
-          name: "用 zTree 方法 异步加载 节点数据",
-          file: "core/async_fun"
-      }, {
-          id: 110,
-          pId: 1,
-          name: "用 zTree 方法 更新 节点数据",
-          file: "core/update_fun"
-      }, {
-          id: 111,
-          pId: 1,
-          name: "单击 节点 控制",
-          file: "core/click"
-      }, {
-          id: 112,
-          pId: 1,
-          name: "展开 / 折叠 父节点 控制",
-          file: "core/expand"
-      }, {
-          id: 113,
-          pId: 1,
-          name: "根据 参数 查找 节点",
-          file: "core/searchNodes"
-      }, {
-          id: 114,
-          pId: 1,
-          name: "其他 鼠标 事件监听",
-          file: "core/otherMouse"
-      }
-  ];
-
-  zNodes = [];
-
   function buildNodes() {
+      var zNodes = [];
+
       // 各等级标签的最新 id
       var levelIds = {
           '0': 0,
@@ -128,7 +44,6 @@
       }
 
       // 遍历 .content 下的所有 h1~h7
-      var lastTagName;
       $(".content")
           .find("h1,h2,h3,h4,h5,h6,h7")
           .each(function(index, domEl) {
@@ -136,7 +51,7 @@
 
               var id = index + 1;
               var pId = levelIds[level - 1];
-              
+
               zNodes.push({
                   id: id,
                   pId: pId,
@@ -149,10 +64,12 @@
               // 更新当前等级的 id
               levelIds[level] = id;
           });
+
+      return zNodes;
   }
 
   $(document).ready(function() {
-      buildNodes()
+      var zNodes = buildNodes();
 
       var t = $("#tree");
       t = $.fn.zTree.init(t, setting, zNodes);
